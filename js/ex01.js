76ae35c7d42ce87530579fff003411f8d756f3b3
nieckasz <165843243+nieckasz@users.noreply.github.com>
2025-08-11 17:55:16 -0300

    Delete js directory

--- js/ex01.js
+++
@@ -1,4 +0,0 @@
-/*Janela pop-up que exibe notificação*/ 
-alert("Alerta apartir de um arquivo externo")
-/* Janela pop-up que pede canfirmação*/
-confirm("3° info tarde de 2025 é o melhor terceiro ano do periodo vespertino de 2025 na area de informatica do ifpr de Irati.confirma?")
